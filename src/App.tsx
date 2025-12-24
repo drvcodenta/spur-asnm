062ad212b3d771d5ba8089abbc1f539eba32bbb5
Dhruv Trivedi <dhrtr.36@gmail.com>
2025-12-24 11:20:39 +0530

    add framer to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { motion } from "framer-motion"
+
 function App() {
 
   return (
     <>
+    <motion.div
+    initial={{ opacity:0.5, scale:0.5}}
+    animate={{ opacity:1, scale:1}}
+    transition={{ duration:0.5}}
+    >
+      this is a frmaer component
+    </motion.div>
       <h1>Vite + React</h1>
     </>
   )
